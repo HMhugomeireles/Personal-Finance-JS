6f3c565ca51905af7fdb9a20c273b31a69c84d27
Hugo Meireles <meireles.hmm@gmail.com>
2019-05-10 00:53:07 +0100

    implement the UI event

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ class App {
   constructor() {
 
     // Elements from DOM
-    this.dateNowElement = document.querySelector('#dateNow');
+    this.dateNowElement = document.querySelector('#dateNow').innerHTML = this.buildData();
     this.listOfMonthElement = document.querySelector('#listOfMonth');
     this.totalAmountElement = document.querySelector('#totalAmount');
+    this.inputId = document.querySelector('#identification');
+    this.inputType = document.querySelector('#type');
+    this.inputDescription = document.querySelector('#description');
+    this.inputAmount = document.querySelector('#amount');
+    this.fieldset = document.querySelector('fieldset');
+    // Buttons
+    this.btnInsert = document.querySelector('#insert');
+    this.btnBack = document.querySelector('#back');
+    this.btnNewRecord = document.querySelector('#newRecord');
+    this.formRecord = document.querySelector('#formRecord');
 
-    this.buildInitDomElements();
+    this.loadData();
+    this.setFormReadOnlyTo(true);
 
     // set events
     this.initEvent();
@@ -20,11 +31,8 @@ class App {
     return day + "/" + month + "/" + year;
   }
 
-  buildInitDomElements() {
-    this.dateNowElement.innerHTML = this.buildData();
-
+  loadData() {
     const listOfMonth = this.getListOfMonth();
-
     let listOutput = "";
     let totalAmount = 0;
 
@@ -50,9 +58,30 @@ class App {
   }
 
   initEvent() {
-
-
+    this.btnNewRecord.addEventListener("click", () => {
+      this.setFormReadOnlyTo(false);
+    });
+    this.btnBack.addEventListener("click", () => {
+      this.setFormReadOnlyTo(true);
+    });
+    this.btnInsert.addEventListener("click", () => {
+      this.setFormReadOnlyTo(true);
+    });
+    this.formRecord.addEventListener("submit", (e) => {
+      console.log(e);
+    });
   }
+
+  setFormReadOnlyTo(setting) {
+    if (setting === true) {
+      this.fieldset.setAttribute("disabled", true);
+      this.btnNewRecord.removeAttribute("disabled", true);
+    } else {
+      this.fieldset.removeAttribute("disabled", false);
+      this.btnNewRecord.setAttribute("disabled", true);
+    }
+  }
+
 
   getListOfMonth() {
     return [
